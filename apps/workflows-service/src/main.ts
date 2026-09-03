b08196eeb3b46cc3846ef79cc106e8a87b319f0f
Walter Ribeiro <wribeiro@testreach.com>
2026-09-03 06:59:05 -0300

    feat: integrate microservices with NATS for workflows communication

--- apps/workflows-service/src/main.ts
+++ apps/workflows-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkflowsServiceModule } from './workflows-service.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(WorkflowsServiceModule);
-  await app.listen(process.env.port ?? 3001);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: process.env.NATS_URL,
+      },
+    },
+    { inheritAppConfig: true },
+  );
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
